6be2942ffa9a62d387349a346424c2fb125dfa86
Heph <hephdotdev@gmail.com>
2020-09-21 04:06:54 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+namespace SecretSanta {
+    class Program {
+        static async Task Main(string[] args) {
+            // Block the main thread
+            await Task.Delay(-1);
+        }
+    }
+}
